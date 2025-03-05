be0bc7772de67747d97b5fc6797b02916705e812
arijbenmerdes <arij2000bms@gmail.com>
2025-03-05 10:34:58 +0100

    test webhook

--- projet-nest-test/src/main.ts
+++ projet-nest-test/src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
+
+
+
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
