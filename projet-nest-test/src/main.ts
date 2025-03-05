bbbaa327d017c7bffcf5281cf6ba4c8b6e9337d2
arijbenmerdes <arij2000bms@gmail.com>
2025-03-06 00:26:18 +0100

    Tester webhook avec ngrok3

--- projet-nest-test/src/main.ts
+++ projet-nest-test/src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
 
-
-
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
